36a23f17b335ae89de99dbb6ddd2b157d9050082
Demian <devaccdemiann@gmail.com>
2019-10-25 15:42:24 +0200

    Added a user function

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'body',
+        'author_id',
+        'allowComments',
+        'premiumPost',
+        'weekNumber',
+        'image',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'author_id');
+    }
+    
 }
