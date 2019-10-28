22b41a07e9919d5c36e425159d5b10eb5b7719b7
Demian <devaccdemiann@gmail.com>
2019-10-28 15:50:21 +0100

    Added expection handler for invalid resource

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,5 +47,10 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json([
+                'error' => 'Resource not found'
+            ], 404);
+        }
     }
 }
