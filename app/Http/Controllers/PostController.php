bbf4a6c706b47ace599f2c5805dd717f6d625c04
Demian <devaccdemiann@gmail.com>
2019-10-28 15:50:56 +0100

    Remade post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,82 @@
 
 namespace App\Http\Controllers;
 
-use App\Post;
 use Illuminate\Http\Request;
+use App\Post;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
-    public function index(Request $request)
-    { }
-    public function post(Request $request)
-    { }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return PostResource::collection(Post::with('user')->paginate(25));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //return  response()->json($request->user()->id);
+
+        $post = Post::create([
+            'user_id' => $request->user()->id,
+            'title' => $request->title,
+            'body' => $request->body,
+            'allowComments' => $request->allowComments,
+            'premiumPost' => $request->premiumPost,
+            'image' => $request->image,
+        ]);
+        dd('test');
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        // check if currently authenticated user is the owner of the book
+        if ($request->user()->id !== $post->user_id) {
+            return response()->json(['error' => 'You can only edit your own post.'], 403);
+        }
+
+        $post->update($request->only(['title', 'description']));
+
+        return new PostResource($post);
+    }
+
+    public function destroy(Request $request, Post $post)
+    {
+        if ($request->user()->id !== $post->user_id) {
+            return response()->json(['error' => 'You can only edit your own post.'], 403);
+        }
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
 }
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'body' => $this->body,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at,
+            'user_id' => $this->user_id,
+            'allowComments' => $this->allowComments,
+            'premiumPost' => $this->premiumPost,
+            'image' => $this->image,
+            'ratings' => $this->ratings,
+        ];
+    }
+}
