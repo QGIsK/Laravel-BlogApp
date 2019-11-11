5db3c12c1560213ec4fefb76147b7ed623dee2b6
Demian <devaccdemiann@gmail.com>
2019-11-11 13:33:30 +0100

    Refactored

--- client/src/components/user/Profile.vue
+++ client/src/components/user/Profile.vue
@@ -7,11 +7,9 @@
             <v-layout fill-height>
               <v-flex xs12 align-end flexbox>
                 <span class="headline">
-                  <!-- <v-btn flat v-if="isUser">Edit</v-btn> -->
                   {{user.name}}
                   <span v-if="user.isEditor">/</span>
-                  <span v-if="user.isAdmin">/</span>
-                  <br />
+                  <br>
                 </span>
               </v-flex>
             </v-layout>
@@ -21,18 +19,11 @@
           <div>
             <span>{{user.about}}</span>
             <span class="grey--text">
-              <br />
+              <br>
               Joined: {{user.join_date | formatDate}}
             </span>
           </div>
         </v-card-text>
-        <v-card-actions class="justify-center">
-          <span v-for="social in user.socials" :key="social.name">
-            <v-btn flat class="mx-2" v-if="social.href" :href="social.href">
-              <span>{{social.name}}</span>
-            </v-btn>
-          </span>
-        </v-card-actions>
       </v-card>
     </v-flex>
     <v-flex xs12 sm4 offset-sm1 mb-5>
@@ -48,7 +39,7 @@
           class="my-4"
         >
           <v-list-tile-avatar>
-            <img :src="post.image" />
+            <img :src="post.image">
           </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>{{ post.title }}</v-list-tile-title>
@@ -73,7 +64,7 @@ export default {
   },
   mounted() {
     this.getUser(this.$route.params.id);
-    this.getUserPosts(this.$route.params.id);
+    // this.getUserPosts(this.$route.params.id);
   },
   computed: {
     isUser() {
@@ -90,15 +81,15 @@ export default {
         this.user = res.data.user;
       });
     },
-    getUserPosts(id) {
-      this.$http({
-        url: `/api/user/${id}/posts`,
-        crossdomain: true,
-        method: "GET"
-      }).then(res => {
-        this.userPosts = res.data.posts;
-      });
-    },
+    // getUserPosts(id) {
+    //   this.$http({
+    //     url: `/api/user/${id}/posts`,
+    //     crossdomain: true,
+    //     method: "GET"
+    //   }).then(res => {
+    //     this.userPosts = res.data.posts;
+    //   });
+    // },
     postRedirect(id) {
       this.$router.push(`/post/${id}`);
     }
