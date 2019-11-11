3643956d485b8cd0a550c9e0aa6e5d4cfddb7a08
Demian <devaccdemiann@gmail.com>
2019-11-11 13:34:15 +0100

    Refactored

--- public/js/app.js
+++ public/js/app.js
@@ -49506,8 +49506,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Script\Desktop\BlogApp-Laravel\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Script\Desktop\BlogApp-Laravel\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Demian\Documents\GitHub\laravel-blog-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Demian\Documents\GitHub\laravel-blog-app\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
